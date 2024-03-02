d927a8643dbb09e3fa88362ef0d61359499be3db
Izabel Nascimento <izabelyale@hotmail.com>
2024-03-02 19:45:51 -0300

    FEAT: Adicionando campos no formulário do Projeto

--- src/components/ProjetoForm/ProjetoForm.jsx
+++ src/components/ProjetoForm/ProjetoForm.jsx
@@ -10,6 +10,10 @@ const schema = yup
   .object({
     nome: yup.string().required(),
     descricao: yup.string().required(),
+    participantes: yup.string().required(),
+    antigosParticipantes: yup.string().required(),
+    orientador: yup.string().required(),
+    scrumMaster: yup.string().required(),
   })
   .required();
 
@@ -32,19 +36,45 @@ const ProjetoForm = () => {
       onSubmit={handleSubmit(handleCadastrarProjeto)}
       className={styles.formContainer}
     >
-      <div>
+      <div className={styles.inputGroup}>
         <InputTextComponent
           name="nome"
           label="Nome"
           placeholder="Digite seu Nome"
           control={control}
         />
-      </div>
-      <div>
         <InputTextComponent
           name="descricao"
           label="Descrição"
           placeholder="descreva aqui..."
+          control={control}
+        />
+      </div>
+      <div className={styles.inputGroup}>
+        <InputTextComponent
+          name="participante"
+          label="Participante"
+          placeholder="Selecione um participante"
+          control={control}
+        />
+        <InputTextComponent
+          name="antigoParticipante"
+          label="Antigo Participante"
+          placeholder="Selecione um participante"
+          control={control}
+        />
+      </div>
+      <div className={styles.inputGroup}>
+        <InputTextComponent
+          name="orientador"
+          label="Orientador"
+          placeholder="Selecione um orientador"
+          control={control}
+        />
+        <InputTextComponent
+          name="scrumMaster"
+          label="Scrum Master"
+          placeholder="Selecione um Scrum Master"
           control={control}
         />
       </div>
