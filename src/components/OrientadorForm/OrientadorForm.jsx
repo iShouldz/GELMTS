e30506a4ed4e20b12b5a06bdcf3ee3bfce898a76
Hester910 <hadassa.ester@ufrpe.br>
2024-03-02 18:19:30 -0300

    FEAT: Ajustando aceitacao de varios orientandos

--- src/components/OrientadorForm/OrientadorForm.jsx
+++ src/components/OrientadorForm/OrientadorForm.jsx
@@ -1,9 +1,9 @@
-/* eslint-disable no-unused-vars */
-import { Button, TextField} from "@mui/material";
+import { Button, TextField, IconButton } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import styles from "./orientadorForm.module.css";
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 import InputTextComponent from "../UI/InputTextComponent/InputTextComponent";
 
 const schema = yup
@@ -11,11 +11,11 @@ const schema = yup
     nome: yup.string().required(),
     cpf: yup.string().required(),
     curso: yup.string().required(),
-    orientando: yup.string().required(),
     especialidade: yup.string().required(),
     projeto: yup.string().required(),
-    login: yup.string().required(),
-    senha: yup.string().required(),
+    orientandos: yup.array().of(
+      yup.string().required()
+    )
   })
   .required();
 
@@ -25,13 +25,34 @@ const OrientadorForm = () => {
     handleSubmit,
     control,
     formState: { errors },
+    watch,
+    setValue
   } = useForm({
     resolver: yupResolver(schema),
+    defaultValues: { orientandos: [''] }
   });
 
   const handleCadastrarOrientador = (data) => {
     console.log(data);
   };
+
+  const orientandos = watch("orientandos");
+
+  const addOrientando = () => {
+    setValue("orientandos", [...orientandos, '']);
+  };
+
+  {Object.keys(errors).length > 0 && (
+    <div className={styles.errorContainer}>
+      <p>Houve alguns erros no formulário:</p>
+      <ul>
+        {Object.keys(errors).map((fieldName, index) => (
+          <li key={index}>{errors[fieldName].message}</li>
+        ))}
+      </ul>
+    </div>
+  )}
+  
 
   return (
     <form
@@ -64,28 +85,34 @@ const OrientadorForm = () => {
         <TextField
           id="outlined-basic"
           label="Especialidade"
-          placeholder="Digite sua especialidade"
+          placeholder="Selecione sua especialidade"
           variant="outlined"
           {...register("especialidade")}
-        />
-      </div>
-
-      <div>
-        <TextField
-          id="outlined-basic"
-          label="Orientando"
-          placeholder="Selecione seu(s) orientando(s)"
-          variant="outlined"
-          {...register("orientando")}
         />
         <TextField
           id="outlined-basic"
           label="Projeto"
-          placeholder="Selecione o projeto"
+          placeholder="Selecione seu projeto"
           variant="outlined"
           {...register("projeto")}
         />
       </div>
+
+      {orientandos.map((orientando, index) => (
+        <div key={index}>
+          <TextField
+            id={`orientandos[${index}]`}
+            label={`Orientando ${index + 1}`}
+            placeholder="Selecione seu(s) orientando(s)"
+            variant="outlined"
+            {...register(`orientandos[${index}]`)}
+          />
+        </div>
+      ))}
+
+      <IconButton onClick={addOrientando}>
+        <PersonAddAlt1Icon />
+      </IconButton>
 
       <div>
         <InputTextComponent
