1abfe0484deaf295f77002a4e8329ae6bc13a56f
Izabel Nascimento <izabelyale@hotmail.com>
2024-03-02 21:29:57 -0300

    FIX: Ajustes de participantes no ProjetoForm

--- src/components/ProjetoForm/ProjetoForm.jsx
+++ src/components/ProjetoForm/ProjetoForm.jsx
@@ -1,17 +1,20 @@
-/* eslint-disable no-unused-vars */
-import { Button, TextField} from "@mui/material";
+import { Button, TextField, IconButton} from "@mui/material";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import styles from "./projetoForm.module.css";
-import InputTextComponent from "../UI/InputTextComponent/InputTextComponent";
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 
 const schema = yup
   .object({
     nome: yup.string().required(),
     descricao: yup.string().required(),
-    participantes: yup.string().required(),
-    antigosParticipantes: yup.string().required(),
+    participantes: yup.array().of(
+      yup.string().required()
+    ),
+    antigosParticipantes: yup.array().of(
+      yup.string().required()
+    ),
     orientador: yup.string().required(),
     scrumMaster: yup.string().required(),
   })
@@ -23,60 +26,117 @@ const ProjetoForm = () => {
     handleSubmit,
     control,
     formState: { errors },
+    watch,
+    setValue
   } = useForm({
     resolver: yupResolver(schema),
+    defaultValues: { participantes: [''], antigosParticipantes: [''] }
   });
 
   const handleCadastrarProjeto = (data) => {
     console.log(data);
   };
 
+  const participantes = watch("participantes");
+  const antigosParticipantes = watch("antigosParticipantes");
+
+  const addParticipante = () => {
+    setValue("participantes", [...participantes, '']);
+  };
+
+  const addAntigoParticipante = () => {
+    setValue("antigosParticipantes", [...antigosParticipantes, '']);
+  };
+
+  {Object.keys(errors).length > 0 && (
+    <div className={styles.errorContainer}>
+      <p>Houve alguns erros no formulário:</p>
+      <ul>
+        {Object.keys(errors).map((fieldName, index) => (
+          <li key={index}>{errors[fieldName].message}</li>
+        ))}
+      </ul>
+    </div>
+  )}
+
   return (
     <form
       onSubmit={handleSubmit(handleCadastrarProjeto)}
-      className={styles.formContainer}
+      className={styles.formProjectContainer}
     >
       <div className={styles.inputGroup}>
-        <InputTextComponent
+        <TextField
           name="nome"
           label="Nome"
           placeholder="Digite seu Nome"
-          control={control}
+          {...register("nome")}
         />
-        <InputTextComponent
+        <TextField
           name="descricao"
           label="Descrição"
           placeholder="descreva aqui..."
-          control={control}
+          {...register("descricao")}
         />
       </div>
       <div className={styles.inputGroup}>
-        <InputTextComponent
+        <TextField
           name="participante"
           label="Participante"
           placeholder="Selecione um participante"
-          control={control}
+          {...register("participante")}
         />
-        <InputTextComponent
-          name="antigoParticipante"
-          label="Antigo Participante"
-          placeholder="Selecione um participante"
-          control={control}
-        />
-      </div>
-      <div className={styles.inputGroup}>
-        <InputTextComponent
-          name="orientador"
-          label="Orientador"
-          placeholder="Selecione um orientador"
-          control={control}
-        />
-        <InputTextComponent
+
+        <TextField
           name="scrumMaster"
           label="Scrum Master"
           placeholder="Selecione um Scrum Master"
-          control={control}
+          {...register("scrumMaster")}
         />
+      </div>
+
+      <div className={styles.inputGroup}>
+        <TextField
+          name="orientador"
+          label="Orientador"
+          placeholder="Selecione um orientador"
+          {...register("orientador")}
+        />
+      </div>
+
+      <div className={styles.inputGroup}>
+        <div>
+          {antigosParticipantes.map((antigoParticipante, index) => (
+            <div key={index} className={styles.participante}>
+              <TextField
+                id={`antigosParticipantes[${index + 1}]`}
+                label={`Antigo Participante ${index + 1}`}
+                placeholder="Selecione um participante"
+                {...register(`antigosParticipantes[${index}]`)}
+              />
+            </div>
+          ))}
+
+          <IconButton onClick={addAntigoParticipante}>
+            <PersonAddAlt1Icon />
+          </IconButton>
+        </div>
+        <div>
+          {participantes.map((participante, index) => (
+            <div key={index} className={styles.participante}>
+              <TextField
+                id={`participantes[${index}]`}
+                label={`Participante ${index + 1}`}
+                placeholder="Selecione um participante"
+                variant="outlined"
+                {...register(`participantes[${index}]`)}
+              />
+            </div>
+          ))}
+
+          <IconButton onClick={addParticipante}>
+            <PersonAddAlt1Icon />
+          </IconButton>
+        </div>
       </div>
 
       <Button type="submit">Enviar</Button>
