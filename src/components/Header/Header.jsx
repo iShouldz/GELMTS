9800398962572a175704ea9995c015e14a36760f
Pedro Souza <pedrosport10@gmail.com>
2024-03-08 20:31:44 -0300

    FIX: Ajustando a sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,11 @@
-import { Button, IconButton, List, ListItem, Typography } from "@mui/material";
+import {
+  Button,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Drawer from "@mui/material/Drawer";
@@ -18,13 +25,14 @@ import MenuIcon from "@mui/icons-material/Menu";
 import HomeIcon from "@mui/icons-material/Home";
 import DeviceHubIcon from "@mui/icons-material/DeviceHub";
 import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 const Header = () => {
   const navigate = useNavigate();
   const [controlSideBar, setControlSideBar] = useState(false);
 
   return (
     <section className={styles.headerMain}>
+      {/*Sidebar fechada */}
       <Drawer variant="permanent">
         <IconButton
           onClick={() => setControlSideBar(true)}
@@ -67,6 +75,18 @@ const Header = () => {
               <LibraryBooksIcon sx={{ fontSize: 33 }} />
             </Typography>
           </ListItem>
+
+          <Divider />
+
+          <ListItem button onClick={() => navigate("/admin")}>
+            <Typography
+              className={styles.SideBarItemContainer}
+              fontWeight="bold"
+              color="primary.main"
+            >
+              <AdminPanelSettingsIcon sx={{ fontSize: 33 }} />
+            </Typography>
+          </ListItem>
         </List>
 
         <ListItem
@@ -85,6 +105,7 @@ const Header = () => {
         </ListItem>
       </Drawer>
 
+      {/*Sidebar aberta */}
       <Drawer open={controlSideBar} onClose={() => setControlSideBar(false)}>
         <div className={styles.logo}>
           <Button onClick={() => navigate("/")}>
@@ -145,33 +166,48 @@ const Header = () => {
           </ListItem>
         </List>
 
-        <List>
-          <ListItem
-            sx={{
-              position: "sticky",
-              bottom: 0,
-              width: "100%",
-              display: "flex",
-              justifyContent: "space-between",
-              backgroundColor: "white",
-              borderTop: "1px solid #e0e0e0",
-              padding: "10px 20px",
-            }}
-          >
-            <Button>
-              <Avatar />
-            </Button>
-            <Typography>Olá, usuario</Typography>
-          </ListItem>
+        <Divider />
 
+        <List sx={{height: '100vh'}}>
+          
           <ListItem button onClick={() => navigate("/admin")}>
             <Typography
               className={styles.SideBarItemContainer}
               fontWeight="bold"
               color="primary.main"
             >
-              <AdminPanelSettingsIcon  sx={{ fontSize: 33 }}/>
+              <AdminPanelSettingsIcon sx={{ fontSize: 33 }} />
               Funções adminstrativas
+            </Typography>
+          </ListItem>
+
+          <ListItem
+            // sx={{
+            //   position: "absolute",
+            //   bottom: 0,
+            //   width: "100%",
+            //   display: "flex",
+            //   justifyContent: "space-between",
+            //   backgroundColor: "white",
+            //   paddingLeft: "0",
+            // }}
+            sx={{
+              position: "absolute",
+              bottom: 0,
+              display: "flex",
+              gap: "25px",
+              justifyContent: "space-between",
+            }}
+          >
+            <Button>
+              <Avatar />
+            </Button>
+            <Typography
+              className={styles.SideBarItemContainer}
+              fontWeight="bold"
+              color="primary.main"
+            >
+              Olá, usuario
             </Typography>
           </ListItem>
         </List>
