1aac722344468ed6d89b6accb6d4cee51af55c21
Hester910 <hadassa.ester@ufrpe.br>
2024-03-07 23:35:21 -0300

    FEAT: Criacao dos modais para gerenciamento de reunioes

---
+++ src/components/ModalUpdateReuniao/ModalUpdateReuniao.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+import { Dialog, DialogContent, DialogTitle} from "@mui/material";
+
+const ModalTemplateReuniao = ({ handleClose, controlDialog, title, component }) => {
+  {/*Instrução: Passe o componente de cadastro para atualizar, so passar com a handle dos dados que funciona */}
+  return (
+    <Dialog onClose={handleClose} open={controlDialog}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        {component}
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ModalTemplateReuniao;
