204500507ce0a6c6c9d1f7589f81cdd44a7b52e5
Pedro Souza <pedrosport10@gmail.com>
2024-03-08 23:11:50 -0300

    FEAT: Melhoria nos modais CRUD

--- src/components/ModalSearch/ModalSearch.jsx
+++ src/components/ModalSearch/ModalSearch.jsx
@@ -9,9 +9,21 @@ import {
   DialogContent,
   DialogTitle,
   List,
-  TextField,
   Typography,
 } from "@mui/material";
+import SelectComponent from "../../components/UI/SelectComponent/SelectComponent";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import InputTextComponent from "../UI/InputTextComponent/InputTextComponent";
+import { cursos } from "../../../utils/lists";
+
+const schema = yup
+  .object({
+    nome: yup.string().required(),
+    curso: yup.string().required(),
+  })
+  .required();
 
 const ModalSearch = ({
   handleClose,
@@ -20,16 +32,61 @@ const ModalSearch = ({
   actionButton,
   actionButtonText,
 }) => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
   return (
-    <Dialog onClose={handleClose} open={controlDialog}>
+    <Dialog
+      onClose={handleClose}
+      open={controlDialog}
+      sx={{
+        "& .MuiDialog-container": {
+          "& .MuiPaper-root": {
+            width: "100%",
+            maxWidth: "50vw",
+          },
+        },
+      }}
+    >
       <DialogTitle>{title}</DialogTitle>
 
       <DialogContent>
-        <Typography fontWeight="bold">Digite o nome do estudante</Typography>
-        <TextField sx={{ width: "30vw", maxWidth: "800px" }} />
-        <List>
+        <form
+          style={{
+            display: "flex",
+            gap: "30px",
+            padding: "20px",
+            justifyContent: "center",
+          }}
+        >
+          <InputTextComponent
+            fontWeight="bold"
+            name="nome"
+            label="CPF"
+            fullWidth
+            placeholder="Digite seu CPF"
+            control={control}
+            helperText="Digite o CPF"
+            sx={{ width: "30vw", maxWidth: "800px" }}
+          />
+          <SelectComponent
+            name="curso"
+            control={control}
+            listagem={cursos}
+            helperText="Selecione o curso"
+          />
+
+          <Button variant="contained">Filtrar</Button>
+        </form>
+
+        <List sx={{ width: "20%" }}>
           {/*Listagem dos estudantes para seleção e depois deleção/update */}
-          <Card sx={{ width: "10vw" }}>
+          <Card >
             <CardMedia sx={{ height: 140 }} image="" title="foto do aluno" />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
--- src/components/UI/SelectComponent/SelectComponent.jsx
+++ src/components/UI/SelectComponent/SelectComponent.jsx
@@ -1,30 +1,33 @@
 /* eslint-disable react/prop-types */
-import { MenuItem, Select } from "@mui/material";
+import { FormHelperText, MenuItem, Select } from "@mui/material";
 import { Controller } from "react-hook-form";
 
-import {estilosMUI} from '../../../../utils/lists'
-const SelectComponent = ({ name, control, listagem = [], ...rest }) => {
+import { estilosMUI } from "../../../../utils/lists";
+const SelectComponent = ({ name, control, listagem = [], helperText='', ...rest }) => {
   return (
     <div>
       <Controller
         name={name}
         control={control}
         render={({ field }) => (
-          <Select
-            {...field}
-            {...rest}
-            select
-            value={field.value || ""}
-            InputLabelProps={{ style: { color: "pink" } }}
-            sx={estilosMUI}
-            onChange={(e) => field.onChange(e.target.value)}
-          >
-            {listagem.map((item) => (
-              <MenuItem key={item} value={item}>
-                {item}
-              </MenuItem>
-            ))}
-          </Select>
+          <>
+            <Select
+              {...field}
+              {...rest}
+              select
+              value={field.value || ""}
+              InputLabelProps={{ style: { color: "pink" } }}
+              sx={estilosMUI}
+              onChange={(e) => field.onChange(e.target.value)}
+            >
+              {listagem.map((item) => (
+                <MenuItem key={item} value={item}>
+                  {item}
+                </MenuItem>
+              ))}
+            </Select>
+            <FormHelperText>{helperText}</FormHelperText>
+          </>
         )}
       />
     </div>
