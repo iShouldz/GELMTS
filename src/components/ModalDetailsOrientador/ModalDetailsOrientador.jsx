4e8498a1d68aad5065dafc576e77145a16b69fc8
Hester910 <hadassa.ester@ufrpe.br>
2024-03-10 12:58:15 -0300

    FEAT: Insercao de imagem do perfil de orientador

---
+++ src/components/ModalDetailsOrientador/ModalDetailsOrientador.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import {
+    Avatar,
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField,
+  } from "@mui/material";
+  import { estilosMUI } from "../../../utils/lists";
+  import avatarOrientador from "../../assets/avatarOrientador.jpg";
+  
+  
