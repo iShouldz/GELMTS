6c5d9463589fbf61676e94de63328681302cf5df
Hester910 <hadassa.ester@ufrpe.br>
2024-03-10 14:31:17 -0300

    FEAT: Adicao da imagem de reuniao e sua insercaoo no modal de detalhes da reuniao

---
+++ src/components/ModalDetailsReuniao/ModalDetailsReuniao.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import {
+    Avatar,
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField,
+  } from "@mui/material";
+  import { estilosMUI } from "../../../utils/lists";
+  import reuniaoPicture from "../../assets/reuniaoPicture.png";  
