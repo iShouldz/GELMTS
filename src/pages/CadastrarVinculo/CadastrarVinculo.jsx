89c5c1e7cd9c4390cd8dd3666189cb425c4db7a5
Pedro Souza <pedrosport10@gmail.com>
2024-03-13 17:17:19 -0300

    FEAT: Criando cadastrar vinculo e seus estilos

---
+++ src/pages/CadastrarVinculo/CadastrarVinculo.jsx
@@ -0,0 +1,35 @@
+import { Typography } from "@mui/material";
+import styles from "./cadastrarVinculo.module.css";
+import VinculoForm from "../../components/VinculoForm/VinculoForm";
+
+const CadastrarVinculo = () => {
+  const handleCadastrar = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <section className={styles.FormularioReuniaoContainer}>
+      <Typography
+        variant="h3"
+        fontWeight="bold"
+        sx={{ display: "flex", justifyContent: "center", marginTop: "10vh" }}
+        color="primary.main"
+      >
+        Formulário de cadastro
+      </Typography>
+
+      <Typography
+        variant="h5"
+        fontWeight="bold"
+        sx={{ display: "flex", justifyContent: "center" }}
+        color="primary.main"
+      >
+        Cadastrar Vinculo
+      </Typography>
+
+      <VinculoForm handleSubmitData={handleCadastrar} cadastro={true} />
+    </section>
+  );
+};
+
+export default CadastrarVinculo;
